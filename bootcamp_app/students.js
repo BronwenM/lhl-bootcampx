b8fabfbdd653e42b3b68f44c5a8ad87627e336db
BronwenM <bronwen.marchant@gmail.com>
2024-10-16 21:25:37 -0700

    sanitized input in teachers.js and students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,18 @@
 const { Pool } = require("pg");
 
-const clArgs = process.argv.slice(2);
+// const clArgs = process.argv.slice(2);
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+    SELECT students.id, students.name AS name, cohorts.name AS cohort
+    FROM students
+    JOIN cohorts ON students.cohort_id = cohorts.id
+    WHERE cohorts.name LIKE $1
+    LIMIT $2;
+    `;
 
 const pool = new Pool({
   user: "development",
@@ -10,15 +22,7 @@ const pool = new Pool({
 });
 
 pool
-  .query(
-    `
-    SELECT students.id, students.name AS name, cohorts.name AS cohort
-    FROM students
-    JOIN cohorts ON students.cohort_id = cohorts.id
-    WHERE cohorts.name LIKE '%${clArgs[0]}%'
-    LIMIT ${clArgs[1] || 5};
-    `
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,20 @@
 const { Pool } = require("pg");
 
-const clArgs = process.argv.slice(2);
+// const clArgs = process.argv.slice(2);
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM assistance_requests
+  JOIN teachers ON assistance_requests.teacher_id = teachers.id
+  JOIN students ON assistance_requests.student_id = students.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teachers.name ASC;
+  `;
 
 const pool = new Pool({
   user: "development",
@@ -9,17 +23,7 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool.query(
-  `
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM assistance_requests
-  JOIN teachers ON assistance_requests.teacher_id = teachers.id
-  JOIN students ON assistance_requests.student_id = students.id
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${clArgs[0] || 'JUL02'}%'
-  ORDER BY teachers.name ASC;
-  `
-)
+pool.query(queryString, values)
 .then((res) => {
   res.rows.forEach((teacher) => {
     console.log(
